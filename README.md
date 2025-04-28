75f040c9d208d925f9b648e0d98b9cd2f2f6b510
Lumine2024 <149254783+Lumine2024@users.noreply.github.com>
2025-04-28 18:33:41 +0800

    chinesization

--- README.md
+++ README.md
@@ -1,41 +1,43 @@
-![logo](https://github.com/craigz28/firmwalker/blob/master/firmwalker-logo.jpg)
 # firmwalker
-A simple bash  script for searching the extracted or mounted firmware file system.
+一个简单的bash脚本，用于搜索提取或挂载的固件文件系统。
 
-It will search through the extracted or mounted firmware file system for things of interest such as:
+它将在提取或挂载的固件文件系统中搜索感兴趣的东西，例如：
 
-* etc/shadow and etc/passwd
-* list out the etc/ssl directory
-* search for SSL related files such as .pem, .crt, etc.
-* search for configuration files
-* look for script files
-* search for other .bin files
-* look for keywords such as admin, password, remote, etc.
-* search for common web servers used on IoT devices
-* search for common binaries such as ssh, tftp, dropbear, etc.
-* search for URLs, email addresses and IP addresses
-* Experimental support for making calls to the Shodan API using the Shodan CLI
+* etc/shadow和etc/passwd
+* 列出etc/ssl目录
+* 搜索SSL相关文件，如.pem，.crt等。
+* 搜索配置文件
+* 查找脚本文件
+* 搜索其他.bin文件
+* 查找关键字，如admin， password， remote等。
+* 搜索物联网设备上使用的常用web服务器
+* 搜索常用的二进制文件，如ssh， tftp， dropbear等。
+* 搜索url，电子邮件地址和IP地址
+* 实验性支持使用Shodan CLI调用Shodan API
 
-## Usage
-* If you wish to use the static code analysis portion of the script, please install eslint: `npm i -g eslint`
-* `./firmwalker {path to root file system} {path for firmwalker.txt}`
-* Example: `./firmwalker linksys/fmk/rootfs ../firmwalker.txt`
-* A file `firmwalker.txt` will be created in the same directory as the script file unless you specify a different filename as the second argument
-* Do not put the firmwalker.sh file inside the directory to be searched, this will cause the script to search itself and the file it is creating
+## 使用
+* 如果你想使用脚本的静态代码分析部分，请安装eslint: 
+* `npm i -g eslint ./firmwalker {源文件系统路径} {firmwalker.txt的路径}`
+* 例如：`./firmwalker linksys/fmk/rootfs ../firmwalker.txt`
+* 文件`firmwalker.txt`将被创建在与脚本文件相同的目录下，除非你指定一个不同的文件名作为第二个参数
+* 不要把`firmwalker.sh`文件放在要搜索的目录中，这会导致脚本搜索自己和它正在创建的文件
 * `chmod 0700 firmwalker.sh`
 
-## How to extend
-* Have a look under 'data' where the checks live or add eslint rules - http://eslint.org/docs/rules/ to eslintrc.json
+## 如何扩展
+* 查看`data`下的检查或添加eslint规则 到eslintrc.json
+* eslint规则：http://eslint.org/docs/rules/
 
-## Example Files - https://1drv.ms/f/s!AucQMYXJNefdvGZyeYt16H72VCLv
-* squashfs-root.zip - contains files from random extracted router firmware. Firmwalker can be run against this file system.
-* rt-ac66u.txt - firmwalker output file
-* xc.txt - firmwalker output file from Ubiquiti device 
-### Script created by Craig Smith and expanded by:
+## 示例文件
+- https://1drv.ms/f/s!AucQMYXJNefdvGZyeYt16H72VCLv
+* squashfs-root.zip - 包含随机提取的路由器固件文件。Firmwalker可以在这个文件系统上运行。
+* rt-ac66u.txt - firmwalker输出文件
+* xc.txt - Ubiquiti设备的firmwalker输出文件
+
+### 由Craig Smith创建并扩展的脚本：
 * Athanasios Kostopoulos
 * misterch0c
 
-### Links
+### 链接
 * https://craigsmith.net
 * https://woktime.wordpress.com
 * https://www.owasp.org/index.php/OWASP_Internet_of_Things_Project#tab=Firmware_Analysis
--- firmwalker.sh
+++ firmwalker.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
 set -e
 set -u
+# 防止输出中文乱码
+export LANG=zh_CN.UTF-8
 
+# 使用方法
 function usage {
-	echo "Usage:"
-	echo "$0 {path to extracted file system of firmware}\
- {optional: name of the file to store results - defaults to firmwalker.txt}"
-	echo "Example: ./$0 linksys/fmk/rootfs/"
+    echo "   firmwalker   "
+	echo "使用方法："
+	echo "$0 {已解压的固件文件路径}\
+ {可选：文件保存的地址，默认为firmwalker.txt}"
+	echo "例如： ./$0 linksys/fmk/rootfs/"
 	exit 1
 }
 
@@ -15,34 +18,34 @@ function msg {
 }
 
 function getArray {
-    array=() # Create array
+    array=()
     while IFS= read -r line
     do
         array+=("$line")
     done < "$1"
 }
 
-# Check for arguments
+# 检测命令行内容，如果只有1个参数或大于3个参数，则输出使用方法并退出
 if [[ $# -gt 2 || $# -lt 1 ]]; then
     usage
 fi
 
-# Set variables
+# 设置变量
 FIRMDIR=$1
 if [[ $# -eq 2 ]]; then
     FILE=$2
 else
     FILE="firmwalker.txt"
 fi
-# Remove previous file if it exists, is a file and doesn't point somewhere
+# 如果原先有相应的文件，将其移除
 if [[ -e "$FILE" && ! -h "$FILE" && -f "$FILE" ]]; then
     rm -f $FILE
 fi
 
-# Perform searches
-msg "***Firmware Directory***"
+# 进行搜索
+msg "***固件地址***"
 msg $FIRMDIR
-msg "***Search for password files***"
+msg "***正在搜索密码文件***"
 getArray "data/passfiles"
 passfiles=("${array[@]}")
 for passfile  in "${passfiles[@]}"
@@ -51,15 +54,15 @@ do
     find $FIRMDIR -name $passfile | cut -c${#FIRMDIR}- | tee -a $FILE
     msg ""
 done
-msg "***Search for Unix-MD5 hashes***"
+msg "***正在搜索Unix-MD5哈希值***"
 egrep -sro '\$1\$\w{8}\S{23}' $FIRMDIR | tee -a $FILE
 msg ""
 if [[ -d "$FIRMDIR/etc/ssl" ]]; then
-    msg "***List etc/ssl directory***"
+    msg "***正在枚举etc/ssl文件夹下的内容***"
     ls -l $FIRMDIR/etc/ssl | tee -a $FILE
 fi
 msg ""
-msg "***Search for SSL related files***"
+msg "***正在搜索SSL相关文件***"
 getArray "data/sslfiles"
 sslfiles=("${array[@]}")
 for sslfile in ${sslfiles[@]}
@@ -68,24 +71,24 @@ do
     find $FIRMDIR -name $sslfile | cut -c${#FIRMDIR}- | tee -a $FILE
        certfiles=( $(find ${FIRMDIR} -name ${sslfile}) )
        : "${certfiles:=empty}"
-       # Perform Shodan search. This assumes Shodan CLI installed with an API key.
+       # 进行Shodan搜索。假设已经通过API秘钥安装了Shodan CLI。
        if [ "${certfiles##*.}" = "pem" ] || [ "${certfiles##*.}" = "crt" ]; then
           for certfile in "${certfiles[@]}"
           do
-             serialno=$(openssl x509 -in $certfile -serial -noout) || echo "Incorrect File Content:Continuing"
+             serialno=$(openssl x509 -in $certfile -serial -noout) || echo "不正确的文件内容：继续"
              serialnoformat=(ssl.cert.serial:${serialno##*=})
              if type "shodan" &> /dev/null ; then
                  shocount=$(shodan count $serialnoformat)
                  if (( $shocount > 0 )); then
-            		msg "################# Certificate serial # found in Shodan ####################"
+            		msg "################# 在Shodan中发现的证书序列号 ####################"
             		echo $certfile | cut -c${#FIRMDIR}- | tee -a $FILE
             		echo $serialno | tee -a $FILE
-                         	echo "Number of devices found in Shodan =" $shocount | tee -a $FILE
+                         	echo "在Shodan中发现" $shocount | tee -a $FILE "个设备"
             		cat $certfile | tee -a $FILE
             		msg "###########################################################################"
                  fi
              else 
-                echo "Shodan cli not found."
+                echo "未找到Shodan CLI"
              fi
           done
        fi
@@ -93,7 +96,7 @@ do
 done
 
 msg ""
-msg "***Search for SSH related files***"
+msg "***正在搜索SSH相关文件***"
 getArray "data/sshfiles"
 sshfiles=("${array[@]}")
 for sshfile in ${sshfiles[@]}
@@ -103,7 +106,7 @@ do
     msg ""
 done
 msg ""
-msg "***Search for files***"
+msg "***正在搜索files文件夹***"
 getArray "data/files"
 files=("${array[@]}")
 for file in ${files[@]}
@@ -113,7 +116,7 @@ do
     msg ""
 done
 msg ""
-msg "***Search for database related files***"
+msg "***正在搜索数据库相关文件***"
 getArray "data/dbfiles"
 dbfiles=("${array[@]}")
 for dbfile in ${dbfiles[@]}
@@ -123,15 +126,15 @@ do
     msg ""
 done
 msg ""
-msg "***Search for shell scripts***"
-msg "##################################### shell scripts"
+msg "***正在搜索shell脚本***"
+msg "##################################### shell脚本"
 find $FIRMDIR -name "*.sh" | cut -c${#FIRMDIR}- | tee -a $FILE
 msg ""
-msg "***Search for other .bin files***"
-msg "##################################### bin files"
+msg "***正在搜索其他.bin文件***"
+msg "##################################### bin文件"
 find $FIRMDIR -name "*.bin" | cut -c${#FIRMDIR}- | tee -a $FILE
 msg ""
-msg "***Search for patterns in files***"
+msg "***正在搜索文件样式***"
 getArray "data/patterns"
 patterns=("${array[@]}")
 for pattern in "${patterns[@]}"
@@ -141,8 +144,8 @@ do
     msg ""
 done
 msg ""
-msg "***Search for web servers***"
-msg "##################################### search for web servers"
+msg "***正在搜索网络服务器***"
+msg "##################################### 网络服务器"
 getArray "data/webservers"
 webservers=("${array[@]}")
 for webserver in ${webservers[@]}
@@ -152,8 +155,8 @@ do
     msg ""
 done
 msg ""
-msg "***Search for important binaries***"
-msg "##################################### important binaries"
+msg "***正在搜索重要二进制文件***"
+msg "##################################### 重要二进制文件"
 getArray "data/binaries"
 binaries=("${array[@]}")
 for binary in "${binaries[@]}"
@@ -164,19 +167,22 @@ do
 done
 
 msg ""
-msg "***Search for ip addresses***"
-msg "##################################### ip addresses"
+msg "***正在搜索IP地址***"
+msg "##################################### IP地址"
 grep -sRIEho '\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b' --exclude-dir='dev' $FIRMDIR | sort | uniq | tee -a $FILE
 
 msg ""
-msg "***Search for urls***"
-msg "##################################### urls"
+msg "***正在搜索URL***"
+msg "##################################### URL"
 grep -sRIEoh '(http|https)://[^/"]+' --exclude-dir='dev' $FIRMDIR | sort | uniq | tee -a $FILE
 
 msg ""
-msg "***Search for emails***"
-msg "##################################### emails"
+msg "***正在搜索邮箱***"
+msg "##################################### 邮箱"
 grep -sRIEoh '([[:alnum:]_.-]+@[[:alnum:]_.-]+?\.[[:alpha:].]{2,6})' "$@" --exclude-dir='dev' $FIRMDIR | sort | uniq | tee -a $FILE
+
+msg ""
+msg "搜索完毕"
 
 #Perform static code analysis 
 #eslint -c eslintrc.json $FIRMDIR | tee -a $FILE
