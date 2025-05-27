81e0b899eacc681eb118d4faea10cc5880ef1627
Lumine2024 <149254783+Lumine2024@users.noreply.github.com>
2025-05-28 01:24:35 +0800

    Update firmwalker.sh

--- firmwalker.sh
+++ firmwalker.sh
@@ -3,9 +3,10 @@ set -u
 # 防止输出中文乱码
 export LANG=zh_CN.UTF-8
 
+
 # 使用方法
 function usage {
-    echo "   firmwalker   "
+	echo "   firmwalker   "
 	echo "使用方法："
 	echo "$0 {已解压的固件文件路径}\
  {可选：文件保存的地址，默认为firmwalker.txt}"
@@ -13,34 +14,36 @@ function usage {
 	exit 1
 }
 
-function msg {
-    echo "$1" | tee -a $FILE
-}
-
-function getArray {
-    array=()
-    while IFS= read -r line
-    do
-        array+=("$line")
-    done < "$1"
-}
-
 # 检测命令行内容，如果只有1个参数或大于3个参数，则输出使用方法并退出
 if [[ $# -gt 2 || $# -lt 1 ]]; then
-    usage
+	usage
 fi
 
 # 设置变量
 FIRMDIR=$1
 if [[ $# -eq 2 ]]; then
-    FILE=$2
+	FILE=$2
 else
-    FILE="firmwalker.txt"
+	FILE="firmwalker.txt"
 fi
 # 如果原先有相应的文件，将其移除
 if [[ -e "$FILE" && ! -h "$FILE" && -f "$FILE" ]]; then
-    rm -f $FILE
+	rm -f $FILE
 fi
+
+function msg {
+	echo "$1" | tee -a $FILE
+}
+
+function getArray {
+	array=()
+	while IFS= read -r line
+	do
+		array+=("$line")
+	done < "$1"
+}
+
+msg "----------------firmwalker----------------"
 
 # 进行搜索
 msg "***固件地址***"
@@ -50,16 +53,16 @@ getArray "data/passfiles"
 passfiles=("${array[@]}")
 for passfile  in "${passfiles[@]}"
 do
-    msg "##################################### $passfile"
-    find $FIRMDIR -name $passfile | cut -c${#FIRMDIR}- | tee -a $FILE
-    msg ""
+	msg "##################################### $passfile"
+	find "$FIRMDIR" -name "$passfile" | sed "s|^${FIRMDIR}||"
+	msg ""
 done
 msg "***正在搜索Unix-MD5哈希值***"
 egrep -sro '\$1\$\w{8}\S{23}' $FIRMDIR | tee -a $FILE
 msg ""
 if [[ -d "$FIRMDIR/etc/ssl" ]]; then
-    msg "***正在枚举etc/ssl文件夹下的内容***"
-    ls -l $FIRMDIR/etc/ssl | tee -a $FILE
+	msg "***正在枚举etc/ssl文件夹下的内容***"
+	ls -l $FIRMDIR/etc/ssl | tee -a $FILE
 fi
 msg ""
 msg "***正在搜索SSL相关文件***"
@@ -67,32 +70,32 @@ getArray "data/sslfiles"
 sslfiles=("${array[@]}")
 for sslfile in ${sslfiles[@]}
 do
-    msg "##################################### $sslfile"
-    find $FIRMDIR -name $sslfile | cut -c${#FIRMDIR}- | tee -a $FILE
-       certfiles=( $(find ${FIRMDIR} -name ${sslfile}) )
-       : "${certfiles:=empty}"
-       # 进行Shodan搜索。假设已经通过API秘钥安装了Shodan CLI。
-       if [ "${certfiles##*.}" = "pem" ] || [ "${certfiles##*.}" = "crt" ]; then
-          for certfile in "${certfiles[@]}"
-          do
-             serialno=$(openssl x509 -in $certfile -serial -noout) || echo "不正确的文件内容：继续"
-             serialnoformat=(ssl.cert.serial:${serialno##*=})
-             if type "shodan" &> /dev/null ; then
-                 shocount=$(shodan count $serialnoformat)
-                 if (( $shocount > 0 )); then
-            		msg "################# 在Shodan中发现的证书序列号 ####################"
-            		echo $certfile | cut -c${#FIRMDIR}- | tee -a $FILE
-            		echo $serialno | tee -a $FILE
-                         	echo "在Shodan中发现" $shocount | tee -a $FILE "个设备"
-            		cat $certfile | tee -a $FILE
-            		msg "###########################################################################"
-                 fi
-             else 
-                echo "未找到Shodan CLI"
-             fi
-          done
-       fi
-    msg ""
+	msg "##################################### $sslfile"
+	find $FIRMDIR -name $sslfile | cut -c${#FIRMDIR}- | tee -a $FILE
+		certfiles=( $(find ${FIRMDIR} -name ${sslfile}) )
+		: "${certfiles:=empty}"
+		# 进行Shodan搜索。假设已经通过API秘钥安装了Shodan CLI。
+		if [ "${certfiles##*.}" = "pem" ] || [ "${certfiles##*.}" = "crt" ]; then
+			for certfile in "${certfiles[@]}"
+			do
+				serialno=$(openssl x509 -in $certfile -serial -noout) || echo "[错误] 证书文件格式不正确，跳过..."
+				serialnoformat=(ssl.cert.serial:${serialno##*=})
+				if type "shodan" &> /dev/null ; then
+					shocount=$(shodan count $serialnoformat)
+					if (( $shocount > 0 )); then
+						msg "################# 在Shodan中发现的证书序列号 ####################"
+						echo $certfile | cut -c${#FIRMDIR}- | tee -a $FILE
+						echo $serialno | tee -a $FILE
+						echo "在Shodan中发现 ${shocount} 个设备" | tee -a $FILE
+						cat $certfile | tee -a $FILE
+						msg "###########################################################################"
+					fi
+				else 
+					echo "未找到Shodan CLI"
+				fi
+			done
+		fi
+	msg ""
 done
 
 msg ""
@@ -101,9 +104,9 @@ getArray "data/sshfiles"
 sshfiles=("${array[@]}")
 for sshfile in ${sshfiles[@]}
 do
-    msg "##################################### $sshfile"
-    find $FIRMDIR -name $sshfile | cut -c${#FIRMDIR}- | tee -a $FILE
-    msg ""
+	msg "##################################### $sshfile"
+	find $FIRMDIR -name $sshfile | cut -c${#FIRMDIR}- | tee -a $FILE
+	msg ""
 done
 msg ""
 msg "***正在搜索files文件夹***"
@@ -111,9 +114,9 @@ getArray "data/files"
 files=("${array[@]}")
 for file in ${files[@]}
 do
-    msg "##################################### $file"
-    find $FIRMDIR -name $file | cut -c${#FIRMDIR}- | tee -a $FILE
-    msg ""
+	msg "##################################### $file"
+	find $FIRMDIR -name $file | cut -c${#FIRMDIR}- | tee -a $FILE
+	msg ""
 done
 msg ""
 msg "***正在搜索数据库相关文件***"
@@ -121,9 +124,9 @@ getArray "data/dbfiles"
 dbfiles=("${array[@]}")
 for dbfile in ${dbfiles[@]}
 do
-    msg "##################################### $dbfile"
-    find $FIRMDIR -name $dbfile | cut -c${#FIRMDIR}- | tee -a $FILE
-    msg ""
+	msg "##################################### $dbfile"
+	find $FIRMDIR -name $dbfile | cut -c${#FIRMDIR}- | tee -a $FILE
+	msg ""
 done
 msg ""
 msg "***正在搜索shell脚本***"
@@ -139,9 +142,9 @@ getArray "data/patterns"
 patterns=("${array[@]}")
 for pattern in "${patterns[@]}"
 do
-    msg "-------------------- $pattern --------------------"
-    grep -lsirnw $FIRMDIR -e "$pattern" | cut -c${#FIRMDIR}- | tee -a $FILE
-    msg ""
+	msg "-------------------- $pattern --------------------"
+	grep -lsirnw $FIRMDIR -e "$pattern" | cut -c${#FIRMDIR}- | tee -a $FILE
+	msg ""
 done
 msg ""
 msg "***正在搜索网络服务器***"
@@ -150,9 +153,9 @@ getArray "data/webservers"
 webservers=("${array[@]}")
 for webserver in ${webservers[@]}
 do
-    msg "##################################### $webserver"
-    find $FIRMDIR -name "$webserver" | cut -c${#FIRMDIR}- | tee -a $FILE
-    msg ""
+	msg "##################################### $webserver"
+	find $FIRMDIR -name "$webserver" | cut -c${#FIRMDIR}- | tee -a $FILE
+	msg ""
 done
 msg ""
 msg "***正在搜索重要二进制文件***"
@@ -161,9 +164,9 @@ getArray "data/binaries"
 binaries=("${array[@]}")
 for binary in "${binaries[@]}"
 do
-    msg "##################################### $binary"
-    find $FIRMDIR -name "$binary" | cut -c${#FIRMDIR}- | tee -a $FILE
-    msg ""
+	msg "##################################### $binary"
+	find $FIRMDIR -name "$binary" | cut -c${#FIRMDIR}- | tee -a $FILE
+	msg ""
 done
 
 msg ""
@@ -176,13 +179,12 @@ msg "***正在搜索URL***"
 msg "##################################### URL"
 grep -sRIEoh '(http|https)://[^/"]+' --exclude-dir='dev' $FIRMDIR | sort | uniq | tee -a $FILE
 
-msg ""
 msg "***正在搜索邮箱***"
 msg "##################################### 邮箱"
-grep -sRIEoh '([[:alnum:]_.-]+@[[:alnum:]_.-]+?\.[[:alpha:].]{2,6})' "$@" --exclude-dir='dev' $FIRMDIR | sort | uniq | tee -a $FILE
+grep -sRIEoh '([[:alnum:]_.-]+@[[:alnum:]_.-]+?\.[[:alpha:].]{2,6})' --exclude-dir='dev' $FIRMDIR | sort | uniq | tee -a $FILE
 
 msg ""
-msg "搜索完毕"
+msg "-------------------搜索完毕-------------------"
 
-#Perform static code analysis 
-#eslint -c eslintrc.json $FIRMDIR | tee -a $FILE
+# 执行静态代码分析
+# eslint -c eslintrc.json $FIRMDIR | tee -a $FILE
